a5c295874c91e8163e2908ee0fe18ce4e4b1938b
Rox87 <rodrigohbx@gmail.com>
2022-01-05 12:15:37 -0300

    setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A function that returns 'world'",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Rox87/python_world",
+    url="https://github.com/Rox87/package",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
